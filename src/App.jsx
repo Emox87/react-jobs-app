fc125ba209392950b17dd612938c50a0d4c5939a
Emil Tsvetanov <tsvetanovemil87@gmail.com>
2026-02-06 00:58:20 +0200

    Instlling react-router-dom and do some styling on the Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+
+
+import Navbar from "./components/Navbar";
+
 function App() {
 
   return (
-    <>
-     <h1 className="bg-indigo-700 text-white p-4 text-2xl">React Jobs Board App</h1>
-    </>
+    <div className="">
+     <Navbar />
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router-dom';
+
 import './index.css'
 import App from './App.jsx'
 
