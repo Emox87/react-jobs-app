d5a356e4b82373078d0b5eae29603f5394f2a4e7
Emil Tsvetanov <tsvetanovemil87@gmail.com>
2026-02-06 17:13:08 +0200

    Adding Card Component and Homepage Page Component

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import {
 
 import MainLayout from "./layouts/MainLayout";
 import NotFoundPage from "./pages/NotFoundPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>,
     ),
