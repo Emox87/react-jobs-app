f1587dc778968241d9f6b5717f298f1e33186758
Emil Tsvetanov <tsvetanovemil87@gmail.com>
2026-02-22 21:40:01 +0200

    Adding JobListings to the HomePage Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import HomeCards from "../components/HomeCards";
+import JobListings from "../components/JobListings";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <HomeCards />
+      <JobListings />
     </>
   );
 };
