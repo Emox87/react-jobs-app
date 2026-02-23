1919a0b39d9ff0e34c23c009470399a996fb8855
Emil Tsvetanov <tsvetanovemil87@gmail.com>
2026-02-23 17:23:11 +0200

    Adding ViewAllJobs Component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import Hero from "../components/Hero";
 import HomeCards from "../components/HomeCards";
 import JobListings from "../components/JobListings";
+import ViewAllJobs from '../components/ViewAllJobs';
 
 const HomePage = () => {
   return (
@@ -8,6 +9,7 @@ const HomePage = () => {
       <Hero />
       <HomeCards />
       <JobListings isHome />
+      <ViewAllJobs />
     </>
   );
 };
